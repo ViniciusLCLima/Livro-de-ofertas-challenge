bf215d6db31d4ee34e89ed13bcbe6394a5875ceb
ViniciusLCLima <87040226+ViniciusLCLima@users.noreply.github.com>
2024-04-26 14:17:37 -0300

    Starting and making reading input.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numInputLines = Int32.Parse(Console.ReadLine());
+for (int i = 1; i <= numInputLines; i++){
+    string line = Console.ReadLine();
+    
+}
